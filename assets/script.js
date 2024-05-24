1efc7838ad8909bdb4805b35abdae9b6859b37c6
David CAVANILLAS <114383991+Cdavid974@users.noreply.github.com>
2024-05-24 10:38:05 +0000

    modified:   assets/script.js 	modified:   index.html

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,20 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+// Sélection des flèches du carrousel
+const leftArrow = document.querySelector("#banner .arrow_left");
+const rightArrow = document.querySelector("#banner .arrow_right");
+
+// Ajout d'un gestionnaire d'événement click à la flèche gauche
+leftArrow.addEventListener("click", () => {
+  console.log("Left arrow clicked!");
+  alert("Left arrow clicked!");
+  changeSlides("prev");
+});
+
+// Ajout d'un gestionnaire d'événement click à la flèche droite
+rightArrow.addEventListener("click", () => {
+  console.log("Right arrow clicked!");
+  alert("Right arrow clicked!");
+  changeSlides("next");
+});
